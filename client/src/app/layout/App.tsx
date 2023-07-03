317b5b12c12509083a45022f136791e0bc44c46f
Ilija Kovaceski <ilija@antweb.io>
2023-07-03 15:02:03 +0200

    End of section 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,6 +3,8 @@ import { Container, CssBaseline, Switch, ThemeProvider, createTheme, } from "@mu
 import Header from "./Header";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
  
@@ -25,6 +27,7 @@ function handleThemeChange()
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
     <CssBaseline/>
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,25 +1,26 @@
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from "@mui/material";
-import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { Product } from "../../app/models/product";
+import agent from "../../app/api/agent";
+import NotFound from "../../app/errors/NotFound";
+import LoadingCompoment from "../../app/layout/LoadingComponent";
 
 export default function ProductDetails() {
-
     const {id} = useParams<{id: string}>();
     const [product, setProduct] = useState<Product | null>(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        axios.get(`http://localhost:5096/api/Products/${id}`)
-        .then(response => setProduct(response.data))
+       id && agent.Catalog.details(parseInt(id))
+        .then(response => setProduct(response))
         .catch(error => console.log(error))
         .finally(()=> setLoading(false))
     }, [id])
 
-    if (loading) return <h3>Loading...</h3>
+    if (loading) return <LoadingCompoment message="Loading product..." />
 
-    if(!product) return <h3>Product not found</h3>
+    if(!product) return <NotFound />
 
 return(
    <Grid container spacing={6}>
@@ -57,8 +58,6 @@ return(
                 </Table>
            </TableContainer>
         </Grid>
-
-
    </Grid>
 )
 }
